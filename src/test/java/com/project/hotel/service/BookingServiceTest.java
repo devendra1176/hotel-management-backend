ee3c58d39d0f39fb4dd6c04e6d7c44a76a1ca6e8
devendra1176 <devendra1176sahu@gmail.com>
2026-04-26 10:59:53 +0530

    test(booking): add create booking test and sucess test cases for BookingService

--- src/test/java/com/project/hotel/service/BookingServiceTest.java
+++ src/test/java/com/project/hotel/service/BookingServiceTest.java
@@ -1,6 +1,8 @@
 package com.project.hotel.service;
 
 import com.project.hotel.dto.BookingRequestDTO;
+import com.project.hotel.dto.BookingResponseDTO;
+import com.project.hotel.entity.Booking;
 import com.project.hotel.entity.Role;
 import com.project.hotel.entity.Room;
 import com.project.hotel.entity.User;
@@ -17,6 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -78,5 +81,64 @@ public class BookingServiceTest {
         //ASSERT extra verification
 
         verify(bookingRepository, never()).save(any());
+    }
+
+    @Test
+    void createBooking_shouldCreateBooking_whenNoOverlap() {
+
+        // ARRANGE
+
+        String email = "dev@mail.com";
+
+        BookingRequestDTO dto = new BookingRequestDTO();
+        dto.setRoomId(1L);
+        dto.setCheckIn(LocalDate.of(2026, 4, 28));
+        dto.setCheckOut(LocalDate.of(2026, 4, 30));
+
+        User user = new User();
+        user.setId(1L);
+        user.setName("Devendra");
+        user.setEmail(email);
+        user.setRole(Role.USER);
+
+        Room room = new Room();
+        room.setId(1L);
+        room.setRoomNumber("101");
+
+        Booking savedBooking = new Booking();
+        savedBooking.setId(10L);
+        savedBooking.setUser(user);
+        savedBooking.setRoom(room);
+        savedBooking.setCheckIn(LocalDate.of(2026, 4, 28));
+        savedBooking.setCheckOut(LocalDate.of(2026, 4, 30));
+
+        when(userRepository.findByEmail(email))
+                .thenReturn(Optional.of(user));
+
+        when(roomRepository.findById(1L))
+                .thenReturn(Optional.of(room));
+
+        when(bookingRepository.existsOverlappingBooking(
+                room,
+                LocalDate.of(2026, 4, 28),
+                LocalDate.of(2026, 4, 30)
+        )).thenReturn(false);
+
+        when(bookingRepository.save(any(Booking.class)))
+                .thenReturn(savedBooking);
+
+        // ACT
+
+        BookingResponseDTO response =
+                bookingService.createBooking(dto, email);
+
+        // ASSERT
+
+        assertEquals(10L, response.getBookingId());
+        assertEquals("101", response.getRoomNumber());
+        assertEquals("2026-04-28", response.getCheckIn());
+        assertEquals("2026-04-30", response.getCheckOut());
+
+        verify(bookingRepository, times(1)).save(any(Booking.class));
     }
 }
