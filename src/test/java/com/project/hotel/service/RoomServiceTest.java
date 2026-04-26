3f9cb3be20ad6711f09fa04b60c9caedb92530bf
devendra1176 <devendra1176sahu@gmail.com>
2026-04-26 12:21:46 +0530

    test(room): add create room success test

--- src/test/java/com/project/hotel/service/RoomServiceTest.java
+++ src/test/java/com/project/hotel/service/RoomServiceTest.java
@@ -1,6 +1,9 @@
 package com.project.hotel.service;
 
+import com.project.hotel.controller.RoomType;
 import com.project.hotel.dto.RoomRequestDTO;
+import com.project.hotel.dto.RoomResponseDTO;
+import com.project.hotel.entity.Room;
 import com.project.hotel.exception.RoomAlreadyExistsException;
 import com.project.hotel.repository.RoomRepository;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -42,5 +46,43 @@ class RoomServiceTest {
 
         // VERIFY
         verify(roomRepository, never()).save(any());
+    }
+
+    @Test
+    void createRoom_shouldCreateRoomSuccessfully() {
+
+        // ARRANGE
+
+        RoomRequestDTO dto = new RoomRequestDTO();
+        dto.setRoomNumber("102");
+        dto.setType("DELUXE");
+        dto.setPrice(2500);
+
+        // Fake saved room (DB will return)
+        Room savedRoom = new Room();
+        savedRoom.setId(1L);
+        savedRoom.setRoomNumber("102");
+        savedRoom.setType(RoomType.DELUXE);
+        savedRoom.setPrice(2500);
+        savedRoom.setAvailable(true);
+
+        when(roomRepository.existsByRoomNumber("102"))
+                .thenReturn(false);
+
+        when(roomRepository.save(any(Room.class)))
+                .thenReturn(savedRoom);
+
+        // ACT
+
+        RoomResponseDTO response =
+                roomService.createRoom(dto);
+
+        // ASSERT
+
+        assertEquals("102", response.getRoomNumber());
+        assertEquals("DELUXE", response.getType());
+        assertEquals(2500, response.getPrice());
+
+        verify(roomRepository, times(1)).save(any(Room.class));
     }
 }
